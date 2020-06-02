8749780f181916c82ca376c18cc379bbfa104152
avengerweb <avengerweb@yandex.ru>
2020-06-02 11:03:25 -0700

    Coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  setupFiles: ['jest-localstorage-mock']
+  setupFiles: ['jest-localstorage-mock'],
+  collectCoverageFrom: [
+    'src/**/*.js'
+  ],
+  collectCoverage: true
 }
