2eab8cfe2afcc7b82f10e5d389fc86ef077553eb
avengerweb <avengerweb@yandex.ru>
2020-06-02 11:14:10 -0700

    cleanup, watch tests directory

--- src/background.js
+++ src/background.js
@@ -20,7 +20,7 @@ function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
     width: 1024,
-    height: 600,
+    height: 680,
     webPreferences: {
     // Use pluginOptions.nodeIntegration, leave this alone
     // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
@@ -65,20 +65,6 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    // Devtools extensions are broken in Electron 6.0.0 and greater
-    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
-    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
-    // If you are not using Windows 10 dark mode, you may uncomment these lines
-    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-    // try {
-    //   await installVueDevtools()
-    // } catch (e) {
-    //   console.error('Vue Devtools failed to install:', e.toString())
-    // }
-
-  }
   createWindow()
 })
 
