7118c008d4d4d0f4eb9a982229c3a0c140150198
avengerweb <avengerweb@yandex.ru>
2020-06-12 12:52:47 -0700

    remove tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,4 @@
-const tailwindcss = require('tailwindcss')
-
 module.exports = {
-  plugins: [
-    tailwindcss
-  ],
   // Disable autoprefixer not needed in this project
   vue: {
     autoprefixer: false
