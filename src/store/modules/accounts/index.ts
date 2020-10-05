ea4149af214f868595e41029cf4fc910ba43f5c0
miga <misha.shibaev.00@yandex.ru>
2020-10-05 18:54:58 +0500

    [LAUN-13]Accounts store finished. Errors store in progress

--- src/store/modules/accounts/index.ts
+++ src/store/modules/accounts/index.ts
@@ -3,6 +3,7 @@ import { RequestStatus } from '@/types/network'
 
 import { IRootState } from '../../types'
 import { IAccount, IAccountsActions, IAccountsGetters, IAccountsState } from '@/store/modules/accounts/types'
+import { axios } from '@/modules/axios'
 
 const state: IAccountsState = {
   accounts: [],
@@ -33,12 +34,55 @@ const mutations: MutationTree<IAccountsState> = {
 }
 
 const actions: IAccountsActions = {
-  async addAccount({ dispatch, commit }, account) {
+  addAccount({ state, commit }, account) {
     if (state.accounts.length === 0) {
       commit('SET_DEFAULT_ID', account.id)
     }
 
+    state.accounts.find(acc => acc.id === account.id) ? console.log(`Account ${account.username} already exist in accounts list`) : commit('ADD_ACCOUNT', account)// TODO: need a toaster
+  },
+  removeAccount({ state, commit }, account) {
+    commit('REMOVE_ACCOUNT', account.id)
 
+    if (state.accounts.length > 0) {
+      if(state.defaultId === account.id) {
+        commit('SET_DEFAULT_ID', state.accounts[0].id)
+      }
+    } else {
+      commit('SET_DEFAULT_ID', 0)
+    }
+  },
+  async sendAuthRequest({ state, dispatch, commit }, {username, password, token}) {
+    const params = {
+      grant_type: 'password',
+      client_id: 3,
+      client_secret: 'W90b7o7e81OI8JwNXItC8GXouS8rUStS9kQPKFul',
+      username,
+      password,
+      token,
+      scope: '*'
+    }
+
+    try {
+      const response = await axios.post('https://api.sirus.su/oauth/token', params)
+      await dispatch('loadAccInfo', response)
+      commit('SET_STATUS', RequestStatus.LOADED)
+    } catch (err) {
+
+    }
+  },
+  async loadAccInfo({ state, dispatch, commit }, authResponse) {
+    const params = {
+      headers: { 'Authorization': `${authResponse.tokenType} ${authResponse.accessToken}` }
+    }
+
+    try {
+      const response = await axios.get('/user', params)
+      await dispatch('addAccount', { ...authResponse, response })
+      commit('SET_STATUS', RequestStatus.LOADED)
+    } catch (err) {
+
+    }
   }
 }
 
--- src/store/modules/accounts/types.ts
+++ src/store/modules/accounts/types.ts
@@ -1,11 +1,16 @@
 import { RequestStatus } from '@/types/network'
-import { ActionContext } from 'vuex'
+import { ActionContext, ActionTree, GetterTree } from 'vuex'
 import { IRootState } from '@/store/types'
 
 export interface IAccount {
   id: number
   username: string
   password: string
+}
+
+export interface IAuthResponse {
+  tokenType: string
+  accessToken: string
 }
 
 export interface IAccountsState {
@@ -18,14 +23,14 @@ export interface IAccountsState {
 
 type ActionCtx = ActionContext<IAccountsState, IRootState>
 
-export interface IAccountsActions {
-  addAccount: (ctx: ActionCtx, payload: IAccount) => Promise<void>
+export interface IAccountsActions extends ActionTree<IAccountsState, IRootState>{
+  addAccount: (ctx: ActionCtx, payload: IAccount) => void
   removeAccount: (ctx: ActionCtx, payload: IAccount) => void
-  loadAccInfo: (ctx: ActionCtx, payload: IAccount) => Promise<void>
+  loadAccInfo: (ctx: ActionCtx, payload: IAuthResponse) => Promise<void>
   sendAuthRequest: (ctx: ActionCtx, payload: {username: string, password: string, token?: string}) => Promise<void>
 }
 
-export interface IAccountsGetters {
+export interface IAccountsGetters extends GetterTree<IAccountsState, IRootState>{
   accounts: (state: IAccountsState) => Array<IAccount>
   defaultAccount: (state: IAccountsState) => IAccount | undefined
 }
