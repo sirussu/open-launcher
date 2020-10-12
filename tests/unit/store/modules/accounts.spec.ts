b0beec69c8bf1c1f0de9109382873b8cea4750a2
miga <misha.shibaev.00@yandex.ru>
2020-10-12 23:40:07 +0500

    [LAUN-13]Broken test for review

---
+++ tests/unit/store/modules/accounts.spec.ts
@@ -0,0 +1,40 @@
+import { createLocalVue } from '@vue/test-utils'
+import Vuex, { Store } from 'vuex'
+import cloneDeep from 'lodash/cloneDeep'
+import nock from 'nock'
+
+import { accountsModule } from '@/store/modules/accounts'
+import { IAccountsState } from '@/store/modules/accounts/types'
+
+import accountsStub from './stubs/accounts.json'
+
+describe('accounts module', () => {
+  let store: Store<{ accounts: IAccountsState }>
+  let localVue
+  const baseURL = 'https://api.sirus.su'
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    store = new Vuex.Store({
+      modules: {
+        accounts: cloneDeep(accountsModule)
+      }
+    })
+
+    nock.cleanAll()
+  })
+
+  test('correct account from request & setting as default', async () => {
+    nock(baseURL).post('/oauth/token').reply(200, accountsStub.account1.tokens)
+    nock(baseURL).get('/api/user').reply(200, accountsStub.account1)
+
+    await store.dispatch('accounts/sendAuthRequest', { username: 'vylor1', password: 'Cbhtytdsq03', token: undefined })
+    console.log(store.getters['accounts/defaultAccount'])
+    expect(Object.keys(store.getters)).toContain('accounts/accounts')
+    expect(Object.keys(store.getters)).toContain('accounts/defaultAccount')
+    expect(store.getters['accounts/accounts']).toHaveLength(1)
+    expect(store.getters['accounts/defaultAccount']).toMatchObject(accountsStub.account1)
+  })
+})
