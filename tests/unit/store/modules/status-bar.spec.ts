aa8f98f7c15a82036e18d0d361c05e65dcab79bc
miga <misha.shibaev.00@yandex.ru>
2020-10-16 15:56:20 +0500

    [LAUN-24]Init

---
+++ tests/unit/store/modules/status-bar.spec.ts
@@ -0,0 +1,38 @@
+import { createLocalVue } from '@vue/test-utils'
+import Vuex, { Store } from 'vuex'
+import cloneDeep from 'lodash/cloneDeep'
+import nock from 'nock'
+
+import { statusBarModule } from '@/store/modules/statusBar'
+import { IStatusState } from '@/store/modules/statusBar/types'
+
+import realmsStub from './stubs/realms.json'
+
+describe('status bar module', () => {
+  let store: Store<{ status: IStatusState }>
+  let localVue
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    store = new Vuex.Store({
+      modules: {
+        status: cloneDeep(statusBarModule)
+      }
+    })
+
+    nock.cleanAll()
+  })
+
+  test('get realms, save them in store', async () => {
+    nock('https://api.sirus.su/api').get('/server/status').reply(200, realmsStub)
+
+    await store.dispatch('getRealms')
+
+    expect(Object.keys(store.getters)).toContain('status/realms')
+    expect(Object.keys(store.getters)).toContain('status/summaryOnline')
+    expect(store.getters['status/realms']).toHaveLength(4)
+    expect(store.getters['status/summaryOnline']).toBe(8764)
+  })
+})
