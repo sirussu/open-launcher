8d050ec000e723b05e585c61a76a9a8d48985ebc
avengerweb <avengerweb@yandex.ru>
2020-10-23 16:26:14 -0700

    Start working on file checking

--- src/services/Files.ts
+++ src/services/Files.ts
@@ -1,5 +1,18 @@
-import { promises as fs } from 'fs'
+import { promises as fs, createReadStream } from 'fs'
 import path from 'path'
+import crypto from 'crypto'
+
+export class FileCheckProgress {
+  public bytes: number = 0
+  public bytesDone: number = 0
+  public path: string | null = null
+
+  constructor(bytes: number, path: string, bytesDone: number) {
+    this.bytes = bytes;
+    this.path = path;
+    this.bytesDone = bytesDone;
+  }
+}
 
 export default class Files {
   static FILES_TO_EXIST = ['Data', 'Data/ruRU']
@@ -40,6 +53,42 @@ export default class Files {
     }
 
     throw new Error('ENOENT: File not found: ' + fileOrDirectory)
+  }
+
+  /**
+   * Return md5 hash of exist file
+   * @param path - path to file
+   * @param onProgress
+   */
+  static async getFileHash(path: string, onProgress?: (progress: FileCheckProgress) => void) {
+    const stats = await fs.stat(path)
+
+    const hash = crypto.createHash('md5')
+    const fileStream = createReadStream(path)
+
+    let bytesDone = 0
+
+    if (onProgress) {
+      onProgress(new FileCheckProgress(stats.size, path, bytesDone))
+    }
+
+    return new Promise((resolve, reject) => {
+      fileStream.on('data', (data) => {
+        hash.update(data)
+        bytesDone += data.length
+        if (onProgress) {
+          onProgress(new FileCheckProgress(stats.size, path, bytesDone))
+        }
+      })
+
+      fileStream.on('end', () => {
+        resolve(hash.digest('hex'))
+      })
+
+      fileStream.on('error', () => {
+        reject()
+      })
+    });
   }
 
   /**
--- tests/unit/services/files.spec.ts
+++ tests/unit/services/files.spec.ts
@@ -1,6 +1,6 @@
 import mock from 'mock-fs'
 
-import Files from '@/services/Files'
+import Files, {FileCheckProgress} from '@/services/Files'
 
 describe('File helper', () => {
   const FILE = mock.file({
@@ -66,6 +66,24 @@ describe('File helper', () => {
         new Date(1).getTime()
       )
     ).resolves.toBeTruthy()
+  })
+
+  it('get file hash', async () => {
+    await expect(
+      Files.getFileHash('/home/user/Name Of Directory/Data/ruRU/patch-9.zip')
+    ).resolves.toBe('f9e71fe2c41a10c0a78218e98a025520')
+  })
+
+  it('get file hash', async () => {
+    const progressMock = jest.fn()
+    expect.assertions(2)
+
+    const path = '/home/user/Name Of Directory/Data/ruRU/patch-9.zip'
+
+    await Files.getFileHash(path, progressMock)
+
+    expect(progressMock).toHaveBeenNthCalledWith(1, new FileCheckProgress(7, path, 0))
+    expect(progressMock).toHaveBeenNthCalledWith(2, new FileCheckProgress(7, path, 7))
   })
 
   it('check file including without timestamp', async () => {
