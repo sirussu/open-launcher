64913117cbdcdaba763595fbf834c0c53e801047
miga <misha.shibaev.00@yandex.ru>
2020-10-30 11:40:43 +0500

    [LAUN-14]Rename constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@
 export const REMOVE_NOTIFICATION_TIMEOUT = 4 * 1000
 export const NOTIFICATION_TITLE = 'Sirus Launcher'
 // accounts
-export const DELAY_TIME_MS = 24 * 60 * 60 * 1000
+export const VERIFY_ACCOUNTS_TIMEOUT = 24 * 60 * 60 * 1000
 
